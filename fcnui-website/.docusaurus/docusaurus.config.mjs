99ee5d28027b6cb34d6b6fdd689f972fe9b5044c
Shohjahon <komiljonovshohjahon@gmail.com>
2024-02-15 18:35:38 +0500

    update docosaurus site name

--- fcnui-website/.docusaurus/docusaurus.config.mjs
+++ fcnui-website/.docusaurus/docusaurus.config.mjs
@@ -4,7 +4,7 @@
  * Modify the docusaurus.config.js file at your site's root instead.
  */
 export default {
-  "title": "My Site",
+  "title": "fcnui Wesbite",
   "tagline": "Dinosaurs are cool",
   "favicon": "img/favicon.ico",
   "url": "https://your-docusaurus-site.example.com",
--- fcnui-website/docusaurus.config.js
+++ fcnui-website/docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'fcnui Wesbite',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
