a897c16de2fdee60f1fda589ec291aae01a2caa6
Shohjahon <komiljonovshohjahon@gmail.com>
2024-02-15 19:14:32 +0500

    fix doco config file

--- fcnui-website/docusaurus.config.js
+++ fcnui-website/docusaurus.config.js
@@ -92,10 +92,7 @@ const config = {
                   {
                     title: 'Docs',
                     items: [
-                      {
-                        label: 'Tutorial',
-                        to: '/docs/intro',
-                      },
+
                     ],
                   },
                   {
