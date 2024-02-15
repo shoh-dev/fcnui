1ff63b18055ebf4d521a7e40ab418b3cb50bfadd
Shohjahon <komiljonovshohjahon@gmail.com>
2024-02-15 21:26:05 +0500

    Update registry

--- registry/releasereg.sh
+++ registry/releasereg.sh
@@ -1,13 +1,13 @@
-#cd playground/
-#
-#rm -rf *
-#
-#cd ../
-#
-#flutter build web
-#
-#cp -r build/web/* ./playground/
-#
+cd playground/
+
+rm -rf *
+
+cd ../
+
+flutter build web
+
+cp -r build/web/* ./playground/
+
 cd ..
 
 sh comreg.sh
